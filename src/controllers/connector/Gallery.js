55ad60229ae3f4e60af221043b7f8215d1a7aeb3
IvanBlacky <tcherivan@gmail.com>
2020-04-26 19:22:14 +0300

    feat: add users claimable reward in getState

--- src/controllers/connector/Gallery.js
+++ src/controllers/connector/Gallery.js
@@ -2,6 +2,7 @@ const core = require('cyberway-core-service');
 const BasicController = core.controllers.Basic;
 const env = require('../../data/env');
 const Mosaic = require('../../models/Mosaic');
+const Gem = require('../../models/Gem');
 const { calculateTracery } = require('../../utils/mosaic');
 
 class Gallery extends BasicController {
@@ -9,8 +10,11 @@ class Gallery extends BasicController {
         super({ connector });
     }
 
-    async getState({ userId, permlink }) {
-        const { mosaics } = await this.getStateBulk({ posts: [{ userId, permlink }] });
+    async getState({ userId, permlink }, { userId: authUserId }) {
+        const { mosaics } = await this.getStateBulk(
+            { posts: [{ userId, permlink }] },
+            { userId: authUserId }
+        );
 
         const mosaic = mosaics[0];
 
@@ -26,14 +30,14 @@ class Gallery extends BasicController {
         };
     }
 
-    async getStateBulk({ posts }) {
+    async getStateBulk({ posts }, { userId }) {
         const traceryContentIdMap = new Map();
 
         for (const { userId, permlink } of posts) {
             traceryContentIdMap.set(calculateTracery(userId, permlink), { userId, permlink });
         }
 
-        const mosaics = await Mosaic.aggregate([
+        const aggregation = [
             {
                 $match: { tracery: { $in: [...traceryContentIdMap.keys()] } },
             },
@@ -49,12 +53,25 @@ class Gallery extends BasicController {
             {
                 $limit: posts.length,
             },
-        ]);
+        ];
+
+        const mosaics = await Mosaic.aggregate(aggregation);
 
         for (const mosaic of mosaics) {
+            if (userId) {
+                const gems = await Gem.find(
+                    { 'contentId.userId': userId, tracery: mosaic.tracery, isClaimable: true },
+                    { reward: 1 },
+                    { lean: true }
+                );
+
+                mosaic.userClaimableReward = gems.reduce(
+                    (totalReward, { reward }) => totalReward + reward,
+                    0
+                );
+            }
             mosaic.isClosed = Date.now() - mosaic.collectionEnd >= 0;
             mosaic.contentId = traceryContentIdMap.get(mosaic.tracery);
-            delete mosaic.tracery;
         }
 
         return {
